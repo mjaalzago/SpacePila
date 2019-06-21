20c62a5b0f398f5aa5abaa4b7ccf417a0c738b8b
Prueba <mjalvarezg@yahoo.com>
2019-06-21 16:21:38 +0200

    Jose- Spaceman

---
+++ Assets/GameAssets/_Scripts/ModoAleatorio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModoAleatorio : MonoBehaviour {
+
+    [SerializeField] float velocidadMovimiento = 5;
+    [SerializeField] float tiempoCambioDireccion = 3;
+    CharacterController miCC;
+
+    void Awake()
+    {
+        miCC = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("CambiarDireccionMovimiento", tiempoCambioDireccion, tiempoCambioDireccion);
+    }
+
+    private void Update()
+    {
+        miCC.SimpleMove(this.transform.forward * velocidadMovimiento);
+    }
+
+    void CambiarDireccionMovimiento()
+    {
+        float rotacionAleatoria = Random.Range(0, 360);
+        Vector3 rotacionAAplicar = new Vector3(0, rotacionAleatoria, 0);
+        this.transform.Rotate(rotacionAAplicar);
+    }
+
+}
